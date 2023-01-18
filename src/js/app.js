f1c5821d92c31f8ab81a12deb1a785a073988f5e
Franco Barrientos <francobarrientos56@gmail.com>
2023-01-18 17:50:45 -0300

    dashboard final

---
+++ src/js/app.js
@@ -0,0 +1,26 @@
+const mobileMenuBtn = document.querySelector("#mobile-menu");
+const cerrarMenu = document.querySelector("#cerrar-menu");
+const sidebar = document.querySelector(".sidebar");
+if(mobileMenuBtn){
+    mobileMenuBtn.addEventListener("click",function(){
+        sidebar.classList.add("mostrar");
+
+    });
+  
+}
+if(cerrarMenu){
+    cerrarMenu.addEventListener("click",function(){
+        sidebar.classList.add("ocultar");
+        setTimeout(() => {
+            sidebar.classList.remove("mostrar");     
+            sidebar.classList.remove("ocultar");     
+        }, 1000);
+    });
+};
+
+window.addEventListener("resize",function(){
+    const anchoPantalla = document.body.clientWidth;
+    if(anchoPantalla >= 768){
+        sidebar.classList.remove("mostrar");
+    }
+})
--- views/templates/sidebar.php
+++ views/templates/sidebar.php
@@ -1,8 +1,21 @@
 <aside class="sidebar">
-    <h2>UpTask</h2>
+    <div class="contenedor-sidebar">
+         <h2>UpTask</h2>
+         <div class="cerrar-menu">
+            <svg id="cerrar-menu" alt="cerrar menu" xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-menu-2" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+            <line x1="4" y1="6" x2="20" y2="6" />
+            <line x1="4" y1="12" x2="20" y2="12" />
+            <line x1="4" y1="18" x2="20" y2="18" />
+            </svg>
+        </div>
+    </div>    
     <nav class="sidebar-nav">
         <a class="<?php echo ($titulo === 'Proyectos') ? 'activo' : '';?>" href="/dashboard">Proyectos</a>
         <a class="<?php echo ($titulo === 'Crear Proyecto') ? 'activo' : '';?>" href="/crear-proyecto">Crear Proyecto</a>
         <a class="<?php echo ($titulo === 'Perfil') ? 'activo' : '';?>" href="/perfil">Perfil</a>
     </nav>
+    <div class="cerrar-sesion-mobile">
+        <a href="/logout" class="cerrar-sesion" >Cerrar Sesion</a>
+    </div>
 </aside>
